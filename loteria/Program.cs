acc6c05d67190b83ed508c85225fe6c292967b6b
OliwiaStrzelczyk <OliwiaStrzelczyk@TE-LOD-D-17S303>
2025-09-23 09:13:41 +0200

    zmain

--- loteria/Program.cs
+++ loteria/Program.cs
@@ -1,6 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
-class Programn
-{
 
+namespace loteria
+{
+    class Programn
+    {
+        static void Main()
+        {
+            Console.WriteLine("Podaj liczbę zestawów do wylosowania ");
+            string nazwa = Console.ReadLine();
+            int ilezestawow = int.Parse(Console.ReadLine());
+            List<int[]> zestaw = new List<int[]>();
+
+            for(int i =0; i<ilezestawow; i++)
+
+            {
+                int[] zestaw = losujzestaw();
+                zestawy.Add(zestaw);
+                Console.WriteLine("Losowanie"+ (i+1) +  ":");
+                Console.WriteLine();
+            }
+
+
+                
+        }
+
+
+    }
 }
