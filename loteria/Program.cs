195472ed0cc4962f1d5fe3ad1ce76e25a74c37b0
OliwiaStrzelczyk <OliwiaStrzelczyk@TE-LOD-D-17S303>
2025-09-23 10:01:40 +0200

    zmiany

--- loteria/Program.cs
+++ loteria/Program.cs
@@ -3,52 +3,59 @@ using System.Collections.Generic;
 
 namespace loteria
 {
-    class Programn
+    class Program
     {
         static void Main()
         {
-            Console.WriteLine("Podaj liczbę zestawów do wylosowania ");
-            string nazwa = Console.ReadLine();
-            int ilezestawow = int.Parse(Console.ReadLine());
-            List<int[]> zestaw = new List<int[]>();
-
-            for(int i =0; i<ilezestawow; i++)
-
+            Console.Write("Podaj liczbę zestawów do wygenerowania losowań: ");
+            int ileZestawow = int.Parse(Console.ReadLine());
+            List<int[]> zestawy = new List<int[]>();
+            for (int i = 0; i < ileZestawow; i++)
             {
-                int[] zestaw = losujzestaw();
+                int[] zestaw = LosujZestaw();
                 zestawy.Add(zestaw);
-                Console.WriteLine("Losowanie"+ (i+1) +  ":");
-              Console.WriteLine("Losowanie " + (i + 1) + ": ");
-               Console.WriteLine(string.Join(" ", zestaw));
+
+                Console.Write("Losowanie " + (i + 1) + ": ");
+                Console.WriteLine(string.Join(" ", zestaw));
+            }
+            int[] wystapienia = PoliczWystapienia(zestawy);
+
+            Console.WriteLine("\nWystąpienia liczb:");
+            for (int i = 1; i <= 49; i++)
+            {
+                if (wystapienia[i] > 0)
+                    Console.WriteLine("Liczba {0}: {1}", i, wystapienia[i]);
+            }
+            Console.WriteLine("\nKoniec programu. Naciśnij Enter...");
+            Console.ReadLine();
         }
-
-        int[] wystapienia = policzwystapienia(zestawy);
-
-        Console.WriteLine("\nWystąpienia liczb:");
-        for (int i = 1; i <= 49; i++)
+        static int[] LosujZestaw()
         {
-            if (wystapienia[i] > 0)
-                Console.WriteLine("Liczba {0}: {1}", i, wystapienia[i]);
-        }
-
-        Console.WriteLine("\nKoniec programu. Naciśnij Enter...");
-        Console.ReadLine();
-    }       
-        static int[] losujzestaw()
-        {
-            Random rnd = new Random ();
+            Random rnd = new Random();
             HashSet<int> liczby = new HashSet<int>();
+
             while (liczby.Count < 6)
             {
-                 
+                int liczba = rnd.Next(1, 49);
+                liczby.Add(liczba);
             }
 
+            int[] wynik = new int[6];
+            liczby.CopyTo(wynik);
+            Array.Sort(wynik);
+            return wynik;
         }
-
-        static int[] policzwystapienia()
+        static int[] PoliczWystapienia(List<int[]> zestawy)
         {
-
+            int[] wystapienia = new int[50]; 
+            foreach (var zestaw in zestawy)
+            {
+                foreach (var liczba in zestaw)
+                {
+                    wystapienia[liczba]++;
+                }
+            }
+            return wystapienia;
         }
-
-     }
- }
+    }
+}
